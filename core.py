636852d8f6caedf53e0823abecd3cf6a7443ebda
LRiosB <36747760+LRiosB@users.noreply.github.com>
2023-11-05 13:58:14 -0300

    Interface do BD

--- core.py
+++ core.py
@@ -9,7 +9,57 @@ class interfaceHash(metaclass=abc.ABCMeta):
     def verificaHash(self, input:str, hash:str) -> bool:
         raise NotImplementedError
 
+class interfaceBD(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def cadastraLogin(self, login, senha, permLvl=0):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def pegaTodosLogins(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def cadastraReview(self, usuario, nota, review):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def cadastraMateria(self, codigoUFMG, nome, nomeProfessor):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def aceitaUsuario(self, userID):
+        raise NotImplementedError
+
+    @abc.abstractmethod    
+    def recusaUsuario(self, userID):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def getListaMaterias(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def getListaReviewsMateria(self, materia):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def cadastraReview(self, usuario, materia, nota, review=""):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def cadastraMateria(self, codigoUFMG, nome, nomeProfessor):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def removeMateria(self, materia):
+        raise NotImplementedError
+    
+    def getUsersPendentes(self):
+        raise NotImplementedError
+
 from hash import implementacaoHash
+from bd import implementacaoBD
+
 
 class interfaceFrontEnd:
 
