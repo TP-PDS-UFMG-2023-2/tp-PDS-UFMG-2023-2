119d15c18306724417bc6045b8762bab0600bb66
Wilgnert de Alcântara <wilgnert@hotmail.com>
2023-11-05 14:06:39 -0300

    Planejamento das paginas

--- showPages.py
+++ showPages.py
@@ -12,15 +12,37 @@ def showPagesAll():
 
 def pagDeslogado():
     #showPagesAll()
+    #show_pages([
+    #    Page("./dccReview.py", "DCC Review"),
+    #    Page("./paginasFrontEnd/login.py", "Login"),
+    #    Page("./paginasFrontEnd/cadastro.py", "Cadastro"),
+    #    Page("./paginasFrontEnd/quemsomos.py", "Quem somos?"),
+    #    Page("./paginasFrontEnd/faq.py", "FAQ")
+    #])
     hide_pages(["Matérias", "Fazer review"])
 
 
 def pagLogado():
     #showPagesAll()
+    #show_pages([
+    #    Page("./dccReview.py", "DCC Review"),
+    #    Page("./paginasFrontEnd/meuPerfil.py", "Meu perfil"),
+    #    Page("./paginasFrontEnd/materias.py", "Matérias"),
+    #    Page("./paginasFrontEnd/fazerReviews.py", "Fazer review"),
+    #    Page("./paginasFrontEnd/quemsomos.py", "Quem somos?"),
+    #    Page("./paginasFrontEnd/faq.py", "FAQ")
+    #])
     hide_pages([])
 
 def pagAdmin():
-    #showPagesAll() 
+    #showPagesAll()
+    #show_pages([
+    #    Page("./dccReview.py", "DCC Review"),
+    #    Page("./paginasFrontEnd/adminValidarUsuario.py", "[Admin] Validar usuário"),
+    #    Page("./paginasFrontEnd/adminCriarRemoverMateria.py", "[Admin] Criar e remover matérias"),
+    #    Page("./paginasFrontEnd/quemsomos.py", "Quem somos?"),
+    #    Page("./paginasFrontEnd/faq.py", "FAQ")
+    #])
     hide_pages(["Matérias", "Fazer review"])
 
 def atualizarEstado(estado:str):
